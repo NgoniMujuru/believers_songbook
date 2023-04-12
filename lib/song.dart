f1ecf0d17ee5b6be9814a0ccd41344bf7b74947a
Ngoni Mujuru <u16285256@tuks.co.za>
2023-04-12 23:52:06 +0200

    feat: audio support test

--- lib/song.dart
+++ lib/song.dart
@@ -17,7 +17,7 @@ class Song extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: Text(songTitle),
@@ -31,47 +31,79 @@ class Song extends StatelessWidget {
                 }),
           ]),
       // backgroundColor: Styles.scaffoldBackground,
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: SizedBox(
-          width: double.infinity,
-          child: DecoratedBox(
-            decoration: const BoxDecoration(
-                // color: Styles.scaffoldBackground,
+      body: Stack(children: [
+        SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Stack(
+            children: [
+              ConstrainedBox(
+                // width: double.infinity,
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height - 60,
+                  minWidth: double.infinity,
                 ),
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Consumer<SongSettings>(builder: (context, songSettings, child) {
-                    return Padding(
-                      padding: MediaQuery.of(context).size.width > 600
-                          ? const EdgeInsets.fromLTRB(80, 20, 16, 40)
-                          : const EdgeInsets.fromLTRB(16, 20, 16, 40),
-                      child: Column(
-                        children: [
-                          const SongPlayer(
-                              audioLink:
-                                  'https://firebasestorage.googleapis.com/v0/b/songbook-for-believers.appspot.com/o/SongAudioFiles%2FBroDonnyReagan-LetMeWalkWithYou%2CJesus-InTheGarden.mp3?alt=media&token=39c22b2c-1518-4060-9619-109011e7afb9'),
-                          if (songSettings.displayKey)
-                            Text(songKey == '' ? '---' : songKey,
-                                style: TextStyle(
-                                    fontSize: songSettings.fontSize,
-                                    fontWeight: FontWeight.bold))
-                          else
-                            const SizedBox(),
-                          Text(songText,
-                              style: TextStyle(fontSize: songSettings.fontSize)),
-                        ],
+
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                      // color: Styles.scaffoldBackground,
                       ),
-                    );
-                  })
-                ],
+                  child: SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Consumer<SongSettings>(builder: (context, songSettings, child) {
+                          return Padding(
+                            padding: MediaQuery.of(context).size.width > 600
+                                ? const EdgeInsets.fromLTRB(80, 20, 16, 40)
+                                : const EdgeInsets.fromLTRB(16, 20, 16, 40),
+                            child: Column(
+                              children: [
+                                if (songSettings.displayKey)
+                                  Text(songKey == '' ? '---' : songKey,
+                                      style: TextStyle(
+                                          fontSize: songSettings.fontSize,
+                                          fontWeight: FontWeight.bold))
+                                else
+                                  const SizedBox(),
+                                Text(songText,
+                                    style: TextStyle(fontSize: songSettings.fontSize)),
+                              ],
+                            ),
+                          );
+                        })
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
-      ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Styles.themeColor.withOpacity(0.0),
+                  Styles.themeColor.withOpacity(0.3),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            // width: ,
+            // full width of screen
+            width: MediaQuery.of(context).size.width,
+            height: 50,
+            child: const SongPlayer(
+                audioLink:
+                    'https://firebasestorage.googleapis.com/v0/b/songbook-for-believers.appspot.com/o/SongAudioFiles%2FBroDonnyReagan-LetMeWalkWithYou%2CJesus-InTheGarden.mp3?alt=media&token=39c22b2c-1518-4060-9619-109011e7afb9'),
+          ),
+        ),
+      ]),
     );
   }
 
--- lib/song_player.dart
+++ lib/song_player.dart
@@ -1,6 +1,8 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 import 'styles.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
 class SongPlayer extends StatefulWidget {
   final String audioLink;
@@ -16,11 +18,32 @@ class SongPlayer extends StatefulWidget {
 
 class _SongPlayerState extends State<SongPlayer> {
   final AudioPlayer _player = AudioPlayer();
+  bool _audioNotLoaded = false;
+  Stream<DurationState>? durationState;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
-    _player.setUrl(widget.audioLink!);
+    initPlayer();
+  }
+
+  initPlayer() async {
+    durationState = Rx.combineLatest2<Duration, PlaybackEvent, DurationState>(
+        _player.positionStream,
+        _player.playbackEventStream,
+        (position, playbackEvent) => DurationState(
+              progress: position,
+              buffered: playbackEvent.bufferedPosition,
+              total: playbackEvent.duration ?? Duration.zero,
+            ));
+    try {
+      await _player.setUrl(widget.audioLink);
+    } catch (e) {
+      print("Error loading audio source: $e");
+      setState(() {
+        _audioNotLoaded = false;
+      });
+    }
   }
 
   @override
@@ -31,19 +54,86 @@ class _SongPlayerState extends State<SongPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return widget.audioLink == '' || _audioNotLoaded
+        ? generateEmptyWidget()
+        : generatePlayer(context);
+  }
+
+  Container generateEmptyWidget() {
+    return Container();
+  }
+
+  StreamBuilder<DurationState> _progressBar() {
+    return StreamBuilder<DurationState>(
+      stream: durationState,
+      builder: (context, snapshot) {
+        final durationState = snapshot.data;
+        final progress = durationState?.progress ?? Duration.zero;
+        final buffered = durationState?.buffered ?? Duration.zero;
+        final total = durationState?.total ?? Duration.zero;
+        return ProgressBar(
+          progress: progress,
+          buffered: buffered,
+          total: total,
+          onSeek: (duration) {
+            setState(() {
+              _player.seek(duration);
+            });
+          },
+          onDragUpdate: (details) {
+            debugPrint('${details.timeStamp}, ${details.localPosition}');
+          },
+          // barHeight: _barHeight,
+          // baseBarColor: _baseBarColor,
+          // progressBarColor: _progressBarColor,
+          // bufferedBarColor: _bufferedBarColor,
+          // thumbColor: _thumbColor,
+          // thumbGlowColor: _thumbGlowColor,
+          // barCapShape: _barCapShape,
+          // thumbRadius: _thumbRadius,
+          // thumbCanPaintOutsideBar: _thumbCanPaintOutsideBar,
+          // timeLabelLocation: _labelLocation,
+          // timeLabelType: _labelType,
+          // timeLabelTextStyle: _labelStyle,
+          // timeLabelPadding: _labelPadding,
+        );
+      },
+    );
+  }
+
+  Row generatePlayer(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        // Playing state.
-        Row(
-          children: [
-            IconButton(
-              icon: Icon(_player.playing ? Icons.pause : Icons.play_arrow),
-              iconSize: 64.0,
-              onPressed: _player.playing ? _player.pause : _player.play,
-            ),
-          ],
+        //progress bar
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: _progressBar(),
+        ),
+
+        IconButton(
+          icon: Icon(_player.playing ? Icons.pause : Icons.play_arrow),
+          iconSize: 32.0,
+          onPressed: () => setState(() {
+            _player.playing ? _player.pause() : _player.play();
+          }),
         ),
       ],
     );
   }
+
+  Text generateAudioLoadingFailed() {
+    return const Text(
+      'Audio loading failed due to network error.',
+      style: TextStyle(color: Styles.themeColor),
+    );
+  }
+}
+
+class DurationState {
+  const DurationState(
+      {required this.progress, required this.buffered, required this.total});
+  final Duration progress;
+  final Duration buffered;
+  final Duration total;
 }
